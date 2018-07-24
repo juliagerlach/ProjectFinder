11087295b40ca6d7d89a82305951b9972f839e56
Julia Gerlach <juliagerlach442@gmail.com>
2018-07-24 13:34:29 -0500

    added registered uer controller and views

---
+++ ProjectFinderApp/ProjectFinderApp/Controllers/RegisteredUserController.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.AspNet.Identity;
+using ProjectFinderApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectFinderApp.Controllers
+{
+    public class RegisteredUserController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: RegisteredUser
+        public ActionResult Index()
+        {
+            return View(db.RegisteredUsers.ToList());
+        }
+
+        // GET: RegisteredUsers/Details/5
+        public ActionResult Details(int? id)
+        {
+            var currentUserId = User.Identity.GetUserId();
+            if (currentUserId == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            RegisteredUser registeredUser = db.RegisteredUsers.Where(r => r.ApplicationUserID == currentUserId).FirstOrDefault();
+            if (registeredUser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(registeredUser);
+        }
+
+        // GET: RegisteredUsers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: RegisteredUsers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,Email,ApplicationUserId")] RegisteredUser registeredUser)
+        {
+            var currentUserId = User.Identity.GetUserId();
+            registeredUser.ApplicationUserID = currentUserId;
+            if (ModelState.IsValid)
+            {
+                db.RegisteredUsers.Add(registeredUser);
+                db.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(registeredUser);
+        }
+
+        // GET: RegisteredUsers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            RegisteredUser registeredUser = db.RegisteredUsers.Find(id);
+            if (registeredUser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(registeredUser);
+        }
+
+        // POST: RegisteredUsers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,Email,ApplicationUserID")] RegisteredUser registeredUser)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(registeredUser).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Details");
+            }
+            return View(registeredUser);
+        }
+
+        // GET: RegisteredUsers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            RegisteredUser registeredUser = db.RegisteredUsers.Find(id);
+            if (registeredUser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(registeredUser);
+        }
+
+        // POST: RegisteredUsers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            RegisteredUser registeredUser = db.RegisteredUsers.Find(id);
+            db.RegisteredUsers.Remove(registeredUser);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
