9ddc546f442ef99869e0d355fd5474747d8ddc41
Julia Gerlach <juliagerlach442@gmail.com>
2018-07-24 12:09:43 -0500

    changed bootstrp theme, finished admins controller

--- ProjectFinderApp/ProjectFinderApp/App_Start/BundleConfig.cs
+++ ProjectFinderApp/ProjectFinderApp/App_Start/BundleConfig.cs
@@ -25,6 +25,10 @@ namespace ProjectFinderApp
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrapSimplex.css",
+                      "~/Content/site.css"));
         }
     }
 }
