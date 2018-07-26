4234ff8a46a0d45094b09f1d07d6753faf513fea
Julia Gerlach <juliagerlach442@gmail.com>
2018-07-26 09:26:21 -0500

    added Saas frontend package

--- ProjectFinderApp/ProjectFinderApp/Models/AccountViewModels.cs
+++ ProjectFinderApp/ProjectFinderApp/Models/AccountViewModels.cs
@@ -70,9 +70,9 @@ namespace ProjectFinderApp.Models
         [Display(Name = "Email")]
         public string Email { get; set; }
 
-        [Display(Name = "UserName")]
+        //[Display(Name = "UserName")]
 
-        public string UserName { get; set; }
+        //public string UserName { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
