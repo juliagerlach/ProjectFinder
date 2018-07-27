cc99df02aacffc7552d778a9f6016bcd258cf7a1
Julia Gerlach <juliagerlach442@gmail.com>
2018-07-27 12:12:57 -0500

    changes to views

---
+++ ProjectFinderApp/ProjectFinderApp/Controllers/MonthlySubscriberController.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.AspNet.Identity;
+using ProjectFinderApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectFinderApp.Controllers
+{
+    public class MonthlySubscriberController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: MonthlySubscribers
+        public ActionResult Index()
+        {
+            return View(db.MonthlySubscribers.ToList());
+        }
+
+        // GET:MonthlySubscribers/Details/5
+        public ActionResult Details(int? id)
+        {
+            var currentUserId = User.Identity.GetUserId();
+            if (currentUserId == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MonthlySubscriber subscriber = db.MonthlySubscribers.Where(r => r.ApplicationUserID == currentUserId).FirstOrDefault();
+            if (subscriber == null)
+            {
+                return HttpNotFound();
+            }
+            return View(subscriber);
+        }
+
+        // GET: MonthlySubscribers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MonthlySubscribers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,Email,ApplicationUserId")] MonthlySubscriber subscriber)
+        {
+            var currentUserId = User.Identity.GetUserId();
+            subscriber.ApplicationUserID = currentUserId;
+            if (ModelState.IsValid)
+            {
+                subscriber.SubscriptionActive = true;
+                subscriber.SubscriptionStartDate = DateTime.Now;
+                subscriber.SubscriptionEndDate = subscriber.SubscriptionStartDate.AddDays(30);
+                db.MonthlySubscribers.Add(subscriber);
+                db.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(subscriber);
+        }
+
+        // GET: MonthlySubscribers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MonthlySubscriber subscriber = db.MonthlySubscribers.Find(id);
+            if (subscriber == null)
+            {
+                return HttpNotFound();
+            }
+            return View(subscriber);
+        }
+
+        // POST: MonthlySubscribers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,Email,ApplicationUserID")] MonthlySubscriber subscriber)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(subscriber).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Details");
+            }
+            return View(subscriber);
+        }
+
+        // GET: MonthlySubscribers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MonthlySubscriber subscriber = db.MonthlySubscribers.Find(id);
+            if (subscriber == null)
+            {
+                return HttpNotFound();
+            }
+            return View(subscriber);
+        }
+
+        // POST: MonthlySubscribers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            MonthlySubscriber subscriber = db.MonthlySubscribers.Find(id);
+            db.MonthlySubscribers.Remove(subscriber);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ProjectFinderApp/ProjectFinderApp/Controllers/YearlySubscriberController.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.AspNet.Identity;
+using ProjectFinderApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectFinderApp.Controllers
+{
+    public class YearlySubscriberController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: YearlySubscribers
+        public ActionResult Index()
+        {
+            return View(db.YearlySubscribers.ToList());
+        }
+
+        // GET:YearlySubscribers/Details/5
+        public ActionResult Details(int? id)
+        {
+            var currentUserId = User.Identity.GetUserId();
+            if (currentUserId == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            YearlySubscriber yearlySubscriber = db.YearlySubscribers.Where(r => r.ApplicationUserID == currentUserId).FirstOrDefault();
+            if (yearlySubscriber == null)
+            {
+                return HttpNotFound();
+            }
+            return View(yearlySubscriber);
+        }
+
+        // GET: YearlySubscribers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: YearlySubscribers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,Email,ApplicationUserId")] YearlySubscriber yearlySubscriber)
+        {
+            var currentUserId = User.Identity.GetUserId();
+            yearlySubscriber.ApplicationUserID = currentUserId;
+            if (ModelState.IsValid)
+            {
+                yearlySubscriber.SubscriptionActive = true;
+                yearlySubscriber.SubscriptionStartDate = DateTime.Now;
+                yearlySubscriber.SubscriptionEndDate = yearlySubscriber.SubscriptionStartDate.AddDays(365);
+                db.YearlySubscribers.Add(yearlySubscriber);
+                db.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(yearlySubscriber);
+        }
+
+        // GET: YearlySubscribers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            YearlySubscriber yearlySubscriber = db.YearlySubscribers.Find(id);
+            if (yearlySubscriber == null)
+            {
+                return HttpNotFound();
+            }
+            return View(yearlySubscriber);
+        }
+
+        // POST: YearlySubscribers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,Email,ApplicationUserID")] YearlySubscriber yearlySubscriber)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(yearlySubscriber).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Details");
+            }
+            return View(yearlySubscriber);
+        }
+
+        // GET: YearlySubscribers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            YearlySubscriber yearlySubscriber = db.YearlySubscribers.Find(id);
+            if (yearlySubscriber == null)
+            {
+                return HttpNotFound();
+            }
+            return View(yearlySubscriber);
+        }
+
+        // POST: YearlySubscribers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            YearlySubscriber yearlySubscriber = db.YearlySubscribers.Find(id);
+            db.YearlySubscribers.Remove(yearlySubscriber);
+            db.SaveChanges();
+            return RedirectToAction("Index"); 
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
