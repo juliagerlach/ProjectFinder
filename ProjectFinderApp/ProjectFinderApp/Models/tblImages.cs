1fec5a451963d66967eef7fd0fce9f977e8c92ff
Julia Gerlach <juliagerlach442@gmail.com>
2018-07-31 15:55:01 -0500

    added images model

---
+++ ProjectFinderApp/ProjectFinderApp/Models/tblImages.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProjectFinderApp.Models
+{
+    public class TblImages
+    {
+        [Key]
+        public int ImageId { get; set; }
+        public string Name { get; set; }
+        public int Size { get; set;}
+        public FileType VarBinary { get; set; }
+
+
+    }
+}
