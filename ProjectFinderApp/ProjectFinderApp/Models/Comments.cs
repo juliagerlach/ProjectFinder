adfe22249ae997215e4e71847529d34d70220e3c
Julia Gerlach <juliagerlach442@gmail.com>
2018-07-31 16:29:01 -0500

    changes to comments model

---
+++ ProjectFinderApp/ProjectFinderApp/Models/Comments.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ProjectFinderApp.Models
+{
+    public class Comments
+    {
+        [Key]
+        public int CommentId { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string Comment { get; set; }
+
+        [ForeignKey("ApplicationUser")]
+        public string UserName { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+    }
+}
